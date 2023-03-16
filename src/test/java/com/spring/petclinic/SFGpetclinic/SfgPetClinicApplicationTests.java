71851c2bc69b0b80277a6a955f63b9a97deb15c7
Bizgis <bojan.majstorovic@gmail.com>
2023-03-16 14:08:51 +0100

    Initial commit

---
+++ src/test/java/com/spring/petclinic/SFGpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.petclinic.SFGpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
